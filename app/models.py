caf2f2567c663df6886924b885b2a7c55dd84f9f
petergt44 <petergt44@gmail.com>
2025-02-28 16:22:27 +0300

    Admin models added

--- app/models.py
+++ app/models.py
@@ -6,7 +6,19 @@ Includes models for accounts, tokens, and API logs.
 from app import db
 from flask_login import UserMixin
 import enum
+from werkzeug.security import generate_password_hash, check_password_hash
 
+
+class Admin(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class RemoteProvider(enum.Enum):
     """Enum for third-party game providers."""
